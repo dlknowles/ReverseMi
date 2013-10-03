57f87a437f35414c23f4a4ecf789acd94abc2117
ctcrrmcidlk <ctcrrmcidlk@7JMTKN1.opm.gov>
2013-10-03 11:01:11 -0400

    changed meta tags

--- header.php
+++ header.php
@@ -1,8 +1,9 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
     <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link href="styles/main.css" rel="stylesheet"/>
         <script src="js/disk.js"></script>
         <script src="js/board.js"></script>
